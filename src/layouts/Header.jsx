1fb364785d7dab79b3846313cddc49f043654376
Nermin Donat <nermindonat@posta.mu.edu.tr>
2025-05-25 21:28:12 +0300

    Search input updated.

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -24,6 +24,7 @@ function Header() {
       const query = search.trim();
       if (query) {
         navigate(`/products?search=${encodeURIComponent(query)}`);
+        setIsMobileMenuOpen(false);
       }
     }
   };
